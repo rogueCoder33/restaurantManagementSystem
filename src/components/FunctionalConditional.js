bcafe80792da449f0d4c2c347818a1240770ff3f
Kunalbhandari <kunalbhandari15@gmail.com>
2023-10-11 14:51:31 +0530

    feat: 5-timer framework addressed review comments
    Signed-off-by: Kunalbhandari <kunalbhandari15@gmail.com>

--- src/components/FunctionalConditional.js
+++ src/components/FunctionalConditional.js
@@ -2,20 +2,13 @@ import React, {useState, useEffect} from "react";
 
 function FunctionConditional(props){
 
-    //const [color, setColor] = useState(props.color);
     const [num, setNum] = useState(0);
 
     useEffect(() => {
         setTimeout(() => {
             setNum((num) => num + 1)
-        }, 4000)
+        }, props.number)
     }, []);
-
-    // function setdelay(){
-    //     setTimeout(() => {
-    //         setColor("violet")
-    //     }, 4000);
-    // }
     
     return(
 
